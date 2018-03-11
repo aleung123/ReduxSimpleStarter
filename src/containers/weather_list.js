96f6c033b4a4671f6670bbfc70c807cc6989a59b
Angela Leung <angelaleung95@gmail.com>
2018-03-10 22:12:52 -0500

    added a table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 
 export default class WeatherList extends Component {
 	render() {
-		
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City<th>
+						<th>Temperature<th>
+						<th>Pressure<th>
+						<th>Humidity<th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		);
 	}
 }
